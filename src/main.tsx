5cdc6f68100e4c38f4e79cf55467313d9693b88b
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2022-11-01 04:02:55 +0530

    -a

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import App from "./App";
 import "./index.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 
+console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV == "production" ? "/sudoku" : "/base");
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <Router>
+        <Router basename={process.env.NODE_ENV == "production" ? "/sudoku" : ""}>
             <App />
         </Router>
     </React.StrictMode>
